bc4dd66738f7661ca6387938eff009d0fc5b3777
Eudes Alves <eudesnoberto@hotmail.com>
2025-06-02 18:33:28 -0300

    Create cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,8 @@
+<form action="salvar.php" method="POST" enctype="multipart/form-data">
+  Nome: <input type="text" name="nome"><br>
+  Data: <input type="date" name="data"><br>
+  Local: <input type="text" name="local"><br>
+  Descrição: <textarea name="descricao"></textarea><br>
+  Imagem: <input type="file" name="imagem"><br>
+  <input type="submit" value="Cadastrar Evento">
+</form>
